8e03e9cde5a351e426eeebce24d53a3e2c843e6d
ellen102 <eavhove@gmail.com>
2018-04-03 15:59:38 +0200

    personalise makePdf

--- makePDF.js
+++ makePDF.js
@@ -1,10 +1,58 @@
 module.paths.push('C:\\Users\\Ellen\\AppData\\Roaming\\npm\\node_modules');
 const puppeteer = require('puppeteer');
+var fs = require('fs');
+var path = require('path');
 
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('file://C:/Users/Ellen/Documents/0 ugent/Thesis/github off/ScratchExercises/web/test.html')
-  await page.pdf({ path: 'book/test.pdf', format: 'A4' })
-  await browser.close()
-})()
+const directory = 'C:/Users/Ellen/Documents/0 ugent/Thesis/github off/ScratchExercises/web'
+
+function makePdf(name) {
+    console.log('generating pdf for ' + name + '...');
+
+    (async() => {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.goto('file://' + directory + '/' + name + '.html')
+        await page.pdf({
+            path: 'book/' + name + '.pdf',
+            format: 'A4'
+        })
+        await browser.close()
+    })()
+
+    console.log('pdf stored in /book/' + name + '.pdf\n');
+}
+
+
+
+if (process.argv.length === 2) {
+    console.log('generate all files in /web\n');
+
+    fs.readdir(directory, function(err, files) {
+        if (err) {
+            console.error("Could not list the directory.", err);
+            process.exit(1);
+        }
+
+        files.forEach(function(file, index) {
+            var filepath = path.join(directory, file);
+            fs.stat(filepath, function(error, stat) {
+                var extension = file.slice(-4);
+                if (stat.isFile() && extension === 'html') {
+                    var name = file.slice(0, -5);
+                    makePdf(name);
+                }
+
+            })
+        });
+    });
+
+
+
+
+} else {
+    for (var i = 2; i < process.argv.length; i++) {
+        console.log(process.argv[i]);
+        var name = process.argv[i];
+        makePdf(name);
+    }
+}
